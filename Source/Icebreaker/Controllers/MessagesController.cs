ded238bb3e4d746e32d0a55413ead49ba2a4c908
Yanna <yanna@users.noreply.github.com>
2020-07-02 01:34:40 -0700

    Add todo for auto optin if we left in an opted out state

--- Source/Icebreaker/Controllers/MessagesController.cs
+++ Source/Icebreaker/Controllers/MessagesController.cs
@@ -434,6 +434,8 @@ namespace Icebreaker
 
                                 var installedTeam = await this.bot.GetInstalledTeam(teamsChannelData.Team.Id);
                                 await this.bot.WelcomeUser(connectorClient, member.Id, tenantId, teamsChannelData.Team.Id, installedTeam.InstallerName);
+
+                                // TODO: if you optout, leave the team, then come back in, we should auto optin again.
                             }
                         }
                     }
